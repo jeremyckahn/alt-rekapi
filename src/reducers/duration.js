77a0dcb28a1b22df9d4784e98af3e43d14a0e68f
Jeremy Kahn <jeremyckahn@gmail.com>
2016-02-10 08:23:07 -0600

    Expand timeline duration with Timeline#keyframe

--- src/reducers/duration.js
+++ src/reducers/duration.js
@@ -1,7 +1,17 @@
+import { ADD_KEYFRAME } from './actors';
 export const initialState = 0;
 
 export default function (state=initialState, action) {
   const { type, name, value } = action;
+
+  switch (type) {
+    case ADD_KEYFRAME:
+      if (action.ms > state) {
+        state = action.ms;
+      }
+
+    break;
+  }
 
   return state;
 }
