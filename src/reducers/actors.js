87c4f09cf6380ba962ed255ad99137b5a236201a
Jeremy Kahn <jeremyckahn@gmail.com>
2016-02-21 13:06:37 -0600

    Move ADD_KEYFRAME logic into its own function

--- src/reducers/actors.js
+++ src/reducers/actors.js
@@ -5,66 +5,83 @@ export const DEFAULT_EASING = 'linear';
 
 export const initialState = fromJS([]);
 
+/**
+ * @param {Object} state
+ * @param {Object} action
+ * @return {Object}
+ */
+function addKeyframe (state, action) {
+  const { ms } = action;
+
+  const indexOfActor = state.findIndex(function (actor) {
+    return actor.id = action.id;
+  });
+
+  var newActor = Map({
+    id: action.id,
+    // FIXME: Implement and test start > 0 scenarios
+    start: 0,
+    end: 0,
+    propertyTracks: {}
+  });
+
+  const newPropertyTracks = Map(action.props).map((value, name) => {
+    const actionEasing = action.easing;
+    var easing;
+
+    if (typeof actionEasing === 'undefined') {
+      easing = DEFAULT_EASING;
+    } else if (typeof actionEasing === 'string') {
+      easing = actionEasing;
+    } else if (typeof actionEasing === 'object') {
+      easing = actionEasing[name] || DEFAULT_EASING;
+    }
+
+    return [{
+      ms: action.ms,
+      easing,
+      name,
+      value
+    }];
+  });
+
+  newActor = newActor.set('propertyTracks', newPropertyTracks);
+
+  if (indexOfActor === -1) {
+    state = state.push(newActor);
+  } else {
+    var existingActor = state.get(indexOfActor);
+    const existingPropertyTracks = existingActor.get('propertyTracks');
+    const newActorPropertyTracks = newActor.get('propertyTracks');
+
+    const mergedPropertyTracks = newActorPropertyTracks.map((value, name) => {
+      var combinedTrack = existingPropertyTracks.get(name) || [];
+      combinedTrack = combinedTrack.concat(value);
+      return combinedTrack.sort(prop => prop.ms);
+    });
+
+    existingActor = existingActor.set('propertyTracks', mergedPropertyTracks);
+
+    // TODO: Try to use a less explicit way to do this
+    existingActor = existingActor.set('end', Math.max(newActor.get('end'), ms));
+
+    state = state.set(indexOfActor, existingActor);
+  }
+
+  return state;
+}
+
+/**
+ * @param {Object} state
+ * @param {Object} action
+ * @return {Object}
+ */
 export default function (state=initialState, action) {
-  const { type, ms } = action;
+  const { type } = action;
 
   switch (type) {
     case ADD_KEYFRAME:
-      const indexOfActor = state.findIndex(function (actor) {
-        return actor.id = action.id;
-      });
-
-      var newActor = Map({
-        id: action.id,
-        // FIXME: Implement and test start > 0 scenarios
-        start: 0,
-        end: 0,
-        propertyTracks: {}
-      });
-
-      const newPropertyTracks = Map(action.props).map((value, name) => {
-        const actionEasing = action.easing;
-        var easing;
-
-        if (typeof actionEasing === 'undefined') {
-          easing = DEFAULT_EASING;
-        } else if (typeof actionEasing === 'string') {
-          easing = actionEasing;
-        } else if (typeof actionEasing === 'object') {
-          easing = actionEasing[name] || DEFAULT_EASING;
-        }
-
-        return [{
-          ms: action.ms,
-          easing,
-          name,
-          value
-        }];
-      });
-
-      newActor = newActor.set('propertyTracks', newPropertyTracks);
-
-      if (indexOfActor === -1) {
-        state = state.push(newActor);
-      } else {
-        var existingActor = state.get(indexOfActor);
-        const existingPropertyTracks = existingActor.get('propertyTracks');
-        const newActorPropertyTracks = newActor.get('propertyTracks');
-
-        const mergedPropertyTracks = newActorPropertyTracks.map((value, name) => {
-          var combinedTrack = existingPropertyTracks.get(name) || [];
-          combinedTrack = combinedTrack.concat(value);
-          return combinedTrack.sort(prop => prop.ms);
-        });
-
-        existingActor = existingActor.set('propertyTracks', mergedPropertyTracks);
-
-        // TODO: Try to use a less explicit way to do this
-        existingActor = existingActor.set('end', Math.max(newActor.get('end'), ms));
-
-        state = state.set(indexOfActor, existingActor);
-      }
-
+      state = addKeyframe(state, action);
       break;
   }
 
